dd0565a99e5c3dad94883f208212c66918c69e1c
Wasit Limprasert <wasit7@gmail.com>
2019-02-06 10:51:25 +0700

    add static

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('current_datetime/', views.current_datetime, name='current_datetime'),
     path('item_list/', views.item_list, name='item_list'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
